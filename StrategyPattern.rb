844106fb88b909049d5541d37a22612574f43098
Hawk1n5 <sgccb266175@gmail.com>
2016-05-09 14:53:25 +0800

    Create StrategyPattern.rb
    StrategyPattern.rb

---
+++ StrategyPattern.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby
+
+class Duck
+  attr_accessor :flyBehavior, :quackBehavior
+  def initialize(fly, quack)
+    @flyBehavior = fly
+    @quackBehavior = quack
+  end
+  def fly
+    @flyBehavior.fly
+  end
+  def quack
+    @quackBehavior.quack
+  end
+end
+
+class FlyWithWins
+  def fly
+    puts "fly.fly.fly"
+  end
+end
+
+class FlyNoWay
+  def fly
+    puts "Can't fly."
+  end
+end
+
+class Quack
+  def quack
+    puts "Quack."
+  end
+end
+
+class NoneQuack
+  def quack
+    puts "......"
+  end
+end
+
+class BlueDuck < Duck
+  def initialize()
+    super(FlyNoWay.new(), Quack.new())
+  end
+end
+
+puts "-----duck1------"
+duck1 = RedDuck.new()
+duck1.fly()
+duck1.quack()
+duck1.quackBehavior = NoneQuack.new()
+duck1.quack()
+puts "-----duck2------"
+duck2 = BlueDuck.new()
+duck2.fly()
+duck2.flyBehavior = FlyWithWins.new()
+duck2.fly()
